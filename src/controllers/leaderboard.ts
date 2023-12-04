864e4ef7c26114b84aa442a97c76e8b5fc4df268
Artem Prokop <artem13.prokop@gmail.com>
2023-12-04 17:52:33 +0100

    Fix /leaderboard

--- src/controllers/leaderboard.ts
+++ src/controllers/leaderboard.ts
@@ -6,22 +6,13 @@ export const leaderboardController = new Composer<CustomContext>()
 leaderboardController.command('leaderboard', async ctx => {
   const members = await getMembers(ctx.db)
 
-  const formatter = new Intl.DateTimeFormat('en-US', {
-    day: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric'
-  })
-
   await ctx.text('leaderboard', {
     members: members
       .map((member, place) =>
         ctx.i18n.t('member', {
           position: place < 3 ? ctx.i18n.t(`place.${place}`) : `${place + 1}.`,
           name: member.name ?? `Anon ${member.id}`,
-          score: member.localScore,
-          day: member.lastStar
-            ? ctx.i18n.t('day', { date: formatter.format(member.lastStar) })
-            : ''
+          score: member.localScore
         })
       )
       .join('\n')
